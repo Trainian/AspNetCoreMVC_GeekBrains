1fb75652ccc3e6ce5c96d79846e328d889ff8d19
Дмитрий Горбовский <dimon007@inbox.ru>
2019-09-01 23:04:37 +0300

    Первое ДЗ, готово

---
+++ AspNetCoreMVC/Controllers/HomeController.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using AspNetCoreMVC.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace AspNetCoreMVC.Controllers
+{
+   public class HomeController : Controller
+   {
+         public static List<EmployeeView> _employees = new List<EmployeeView>
+         {
+            new EmployeeView
+            {
+               id = 1,
+               Name = "Дмитрий",
+               Surname = "Горбовский",
+               Patronymic = "Владмирович",
+               BirthDate = new DateTime(1991,03,28)
+            },
+            new EmployeeView
+            {
+               id = 2,
+               Name = "Алина",
+               Surname = "Котар",
+               Patronymic = "Сергеевна",
+               BirthDate = new DateTime(1994,07,22)
+            }
+         };
+        public IActionResult Index()
+        {
+            return View(_employees);
+        }
+        public IActionResult ViewModel(int id)
+        {
+            var employee = _employees.FirstOrDefault(t => t.id == id);
+            if (ReferenceEquals(employee, null))
+               return NotFound();
+            return View(employee);
+        }
+   }
+}
---
+++ AspNetCoreMVC/Models/EmployeeView.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace AspNetCoreMVC.Models
+{
+   public class EmployeeView
+   {
+      public int id { get; set; }
+      public string Name { get; set; }
+      public string Surname { get; set; }
+      public string Patronymic { get; set; }
+      public DateTime BirthDate { get; set; }
+   }
+}
